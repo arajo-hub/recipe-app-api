e41e107d04c31e0af5af98c6f328971eca859c8d
arajo-hub <joara9566@naver.com>
2020-08-09 09:30:30 +0900

    Added list recipe feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 class TagSerializer(serializers.ModelSerializer):
     """Serializer for tag objects"""
@@ -10,9 +10,27 @@ class TagSerializer(serializers.ModelSerializer):
         read_only_fields=('id', )
 
 class IngredientSerializer(serializers.ModelSerializer):
-    """serializer for ingredient objects"""
+    """Serializer for ingredient objects"""
 
     class Meta:
         model=Ingredient
         fields=('id', 'name')
         read_only_fields=('id',)
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serialize a recipe"""
+
+    ingredients=serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredient.objects.all()
+    )
+    tags=serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
+    class Meta:
+        model=Recipe
+        fields=(
+                'id', 'title', 'ingredients', 'tags', 'time_minutes',
+                'price', 'link')
+        read_only_fields=('id', )
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 from recipe import serializers
 
 class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
@@ -29,3 +29,14 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingredients in the database"""
     queryset=Ingredient.objects.all()
     serializer_class=serializers.IngredientSerializer
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipes in the database"""
+    serializer_class=serializers.RecipeSerializer
+    queryset=Recipe.objects.all()
+    authentication_classes=(TokenAuthentication,)
+    permission_classes=(IsAuthenticated, )
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user)
