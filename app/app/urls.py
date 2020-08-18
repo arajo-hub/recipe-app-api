52f8fbd670cc4e379af065bb8ac0eee721b99ce8
arajo-hub <joara9566@naver.com>
2020-08-18 23:28:46 +0900

    API call

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls'))
